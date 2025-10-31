cc642edddf9bd9de54af3446ba08d9c3a34372be
DevDaniel21 <danielglaucia2.007@gmail.com>
2025-10-31 17:12:29 -0300

    test: add user get by id

--- test/users.test.js
+++ test/users.test.js
@@ -7,12 +7,12 @@ const userRoutes = require('../routes/users');
 app.use(express.json());
 app.use('/api/users', userRoutes);
 
-describe('POST - cria um usuário', () => {
+describe('Usuários', () => {
     let idUser = uuidv4();
 
     // setup do test => preparação
     beforeAll(async () => {
-        let userId = 1; // id é auto incremento 
+        let userId = 1; // id é auto incremento
         await request(app).delete(`/api/users/${userId}`);
     });
 
@@ -25,26 +25,29 @@ describe('POST - cria um usuário', () => {
     it('Deve criar um usuário', async () => {
         // let idUser = uuidv4(); // => transferir para o escopo acima /pai
         let userData = {
-            "email": `${idUser}@example.com`,
-            "name": `Usuario ${idUser}`,
-            "password": "12345678"
-        }
+            email: `${idUser}@example.com`,
+            name: `Usuario ${idUser}`,
+            password: '12345678',
+        };
         const user = await request(app).post('/api/users').send(userData);
         console.log(user.body);
         expect(user.statusCode).toBe(201);
         expect(user.body.email).toBe(`${idUser}@example.com`);
-
     });
 
     it('Novo usuário deve realizar login', async () => {
         let userLogin = {
-            "email": `${idUser}@example.com`,
-            "password": "12345678"
-        }
-        const response = await request(app).post('/api/users/login').send(userLogin);
+            email: `${idUser}@example.com`,
+            password: '12345678',
+        };
+        const response = await request(app)
+            .post('/api/users/login')
+            .send(userLogin);
         expect(response.statusCode).toBe(200);
     });
 
+    it('Obter o usuário 1 pelo ID', async () => {
+        const response = await request(app).get('/api/users/1');
+        expect(response.statusCode).toBe(200);
+    });
 });
-
-
